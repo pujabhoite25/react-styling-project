c0365825a3f976c45e6ad4e4276bc9c8d1af1ee8
Puja Bhoite <pujabhoite25@gmail.com>
2023-02-17 12:21:15 +0530

    Using classes for synamic styles of validation

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -25,9 +25,9 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label style={{ color: isValid ? 'black' : 'orange' }}>Course Goal</label>
-        <input style={{ borderColor: isValid ? 'black' : 'orange', backgroundColor: isValid ? 'transparent' : 'orange' }} type="text" onChange={goalInputChangeHandler} />
+      <div className={`form-control ${isValid? '' : 'invalid'}`}>
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
